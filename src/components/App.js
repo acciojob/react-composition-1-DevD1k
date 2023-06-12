45cc1ec363652b1f1e6f549081a9be390dd21df7
Devdutt Mehrol <devduttmehrol666@gmail.com>
2023-06-12 23:04:23 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Hello Finally</h1>
+      {/* Do not remove the main div */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
